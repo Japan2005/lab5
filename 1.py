e4380f2d161d03857d25acefe55b389ddca02ba5
Vichaya Roongsiripornphol <j@Vichayas-MacBook-Pro.local>
2025-02-06 15:11:13 +0700

    Your commit message

--- 1.py
+++ 1.py
@@ -7,32 +7,39 @@ class Simple_drawing_window(QWidget):
     def __init__(self):
         QWidget.__init__ (self, None)
         self.setWindowTitle("Simple Drawing")
-        self.rabbit = QPixmap(r"images/rabbit.png")
+        self.rabbit = QPixmap(r"SEP/lab5/rabbit.png")
         
     def paintEvent(self, e):
         p = QPainter()
         p.begin(self)
-        p.setPen(QColor(0, 0, 0))
-        p.setBrush(QColor(0, 127, 0))
-        p.drawPolygon(
-            [QPoint(70,100), QPoint(100, 110),
-             QPoint(130, 100), QPoint(100, 150),]
-        )
+    
+        p.setPen(QColor(0, 0, 0))  
+        
+        p.setBrush(QColor(255, 255, 255))  
 
-        p.setPen(QColor (255, 127, 0)) 
-        p.setBrush(QColor (255, 127, 0))
-        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
+        p.drawRect(70, 150, 100, 100)
+
+        p.setBrush(QColor(255, 0, 0)) 
+        p.drawPolygon([
+            QPoint(70, 150), QPoint(170, 150), QPoint(120, 100)
+        ])
         
-        p.drawPolygon(
-        [QPoint(50, 200), QPoint(150, 200), QPoint(100, 400) ,])
+        p.setBrush(QColor(100, 50, 0))  
+        p.drawRect(110, 200, 20, 50)
         
-        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit) 
+        p.setBrush(QColor(0, 255, 255))  
+        p.drawRect(80, 170, 20, 20)
+        p.drawRect(140, 170, 20, 20)
+
+        p.drawPixmap(QRect(220, 140, 100, 100), self.rabbit) 
+
+
         p.end()
         
 def main():
     app = QApplication(sys.argv)
     w = Simple_drawing_window()
-    w. show()
+    w.show()
     return app.exec()
 
 if __name__ == "__main__":
